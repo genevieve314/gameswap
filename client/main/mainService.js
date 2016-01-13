f4113203a56701be781dadb15550b66ecdb8cee4
Ryan Reynolds <reynoldsryanscott@gmail.com>
2016-01-13 15:44:36 -0800

    [feature] added basic messaging between users

--- client/main/mainService.js
+++ client/main/mainService.js
@@ -2,11 +2,17 @@ angular
   .module('main.service', [])
   .service('MainService', function($http) {
   this.searchOfferings = function(game){
-    $http.post('/searchofferings', game)
+    return $http.post('/searchofferings', game)
     .then(function(resp){
       return resp.data;
     }.bind(this), function(error) {
       throw error;
     }.bind(this));
+  };
+  this.addRecipient = function(recipient) {
+    this.recipient = recipient;
+  };
+  this.getRecipient = function() {
+    return this.recipient;
   };
 });
---
+++ client/messages/messagesService.js
@@ -0,0 +1,20 @@
+angular
+.module('messages.service', [])
+.service('MessagesService', function() {
+  this.sendMessage = function(message){
+    return $http.post('/addmessage', message)
+    .then(function(resp){
+      return resp.data;
+    }.bind(this), function(error) {
+      throw error;
+    }.bind(this));
+  };
+  this.getMessages = function() {
+    return $http.get('/getmessages')
+    .then(function(resp) {
+      return resp.data;
+    }.bind(this), function(error) {
+      throw error;
+    }.bind(this));
+  };
+});
