95189162df54f148df4d27cfe4fba34912cd573e
Ryan Reynolds <reynoldsryanscott@gmail.com>
2016-01-13 20:17:24 -0800

    [refactor] added TODOs for swapping and messaging functionality

--- client/main/mainController.js
+++ client/main/mainController.js
@@ -11,13 +11,13 @@ angular
       this.psGames = [];
       this.xboxGames = [];
       for(var i = 0; i < this.results.length; i++) {
-        if(this.results[i].game.name.toLowerCase() === game.toLowerCase()) {
-          if(this.results[i].game.platform === 'Playstation 4') {
+        if(this.results[i].title.toLowerCase() === game.toLowerCase()) {
+          if(this.results[i].platform === 'Playstation 4') {
             if(this.hasPlaystation === false) {
               this.hasPlaystation = true;
             }
             this.psGames.push(this.results[i]);
-          } else if(this.results[i].game.platform === 'Xbox One') {
+          } else if(this.results[i].platform === 'Xbox One') {
             if(this.hasPlaystation === false) {
               this.hasXbox = true;
             }
@@ -41,56 +41,7 @@ angular
     MainService.addRecipient(recipient);
   };
 
-  //TODO: database version
-  // this.searchGames = function(game) {
-  //   MainService.searchGamesDb(game).
-  //   then(function() {
-  //
-  //   });
-  // };
+  //TODO: get currentUser's seeking games from profile data and then compare against every game in the offering table
+  //TODO: THEN iterate through every result and check what that result's user is seeking
+    //if it matches what currentUser is offering then add to final results list to display all possible swaps
 });
-
-//hardcoded for now to test
-var games = [
-  {
-    name: 'Fallout 4',
-    score: 100,
-    platform: 'Playstation 4',
-    image: 'http://media.moddb.com/images/games/1/42/41925/fallout4-box.jpg'
-  },
-  {
-    name: 'Fallout 4',
-    score: 90,
-    platform: 'Xbox One',
-    image: 'http://cdnau.ibtimes.com/sites/au.ibtimes.com/files/styles/v2_article_large/public/2015/07/03/fallout-4.jpg'
-  },
-  {
-    name: 'Test',
-    score: 90,
-    platform: 'Xbox One',
-    image: ''
-  },
-];
-
-var users = [
-  {
-    name: 'Ryan',
-    game: this.games[0],
-    distance: 10
-  },
-  {
-    name: 'Tom',
-    game: this.games[0],
-    distance: 25
-  },
-  {
-    name: 'Erik',
-    game: this.games[1],
-    distance: 10
-  },
-  {
-    name: 'Genevieve',
-    game: this.games[1],
-    distance: 25
-  }
-];
