ab72236ca194a0faaba2c9b9e4871fd9981141f1
Erik McGarraugh <erikmcgarraugh@gmail.com>
2016-01-14 15:57:21 -0800

    [chore]fixed conflict

--- database/database.js
+++ database/database.js
@@ -21,26 +21,26 @@ tables.create()
 //   connection = mysql.createConnection(db_config);
 //   // Recreate the connection, since the old one cannot be reused.
 
-//   connection.connect(function(err) {              
+//   connection.connect(function(err) {
 //   // The server is either down
-//     if(err) {                                     
+//     if(err) {
 //     // or restarting (takes a while sometimes).
 //       console.log('error in db handleDisconnect:', err);
-//       setTimeout(handleDisconnect, 2000); 
+//       setTimeout(handleDisconnect, 2000);
 //       // We introduce a delay before attempting to reconnect,
-//     }                                     
+//     }
 //     // to avoid a hot loop, and to allow our node script to
-//   });                                    
+//   });
    // process asynchronous requests in the meantime. If you're also serving http, display a 503 error.
   // connection.on('error', function(err) {
   //   console.log('db error', err);
-  //   if(err.code === 'PROTOCOL_CONNECTION_LOST') { 
+  //   if(err.code === 'PROTOCOL_CONNECTION_LOST') {
   //   // Connection to the MySQL server is usually
-  //     handleDisconnect();                         
+  //     handleDisconnect();
   //     // lost due to either server restart, or a
-  //   } else {                                      
+  //   } else {
   //   // connnection idle timeout (the wait_timeout
-  //     throw err;                                  
+  //     throw err;
   //     // server variable configures this)
   //   }
   // });
@@ -203,5 +203,3 @@ module.exports = {
     })
   }
 }
-
-
