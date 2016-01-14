7b23c39fceb512f21b5d0b745bd6c748a8aae0df
Genevieve <home@Genevieve-Kolves-MacBook-Pro.local>
2016-01-13 16:17:54 -0800

    [feature] adds messages querie to database

--- database/test_db.js
+++ database/test_db.js
@@ -10,7 +10,6 @@ app.listen(PORT,function(){
   console.log('listening on port 3000');
 });
 
-
 db.addUser('test@test.com', 'somedude', 'hashed', function (data){
 	console.log('add user: ', data);
 	return data;
