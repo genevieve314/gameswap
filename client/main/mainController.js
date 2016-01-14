ebe9acc983a6ac77ffa5bab89b6cca1f2664ec18
Ryan Reynolds <reynoldsryanscott@gmail.com>
2016-01-14 15:49:33 -0800

    [feature] added matching games offered by user with games sought by other users functionality

--- client/main/mainController.js
+++ client/main/mainController.js
@@ -29,6 +29,25 @@ angular
       console.error(error);
     }.bind(this));
   };
+  this.searchSeeking = function(game) {
+    MainService.searchSeeking({game: game})
+    .then(function(data) {
+      this.resultsSeeking = data.results;
+      this.psGamesSeeking = [];
+      this.xboxGamesSeeking = [];
+      for(var i = 0; i < this.resultsSeeking.length; i++) {
+        if(this.resultsSeeking[i].title.toLowerCase() === game.toLowerCase()) {
+          if(this.resultsSeeking[i].platform === 'Playstation 4') {
+            this.psGamesSeeking.push(this.resultsSeeking[i]);
+          } else if(this.resultsSeeking[i].platform === 'Xbox One') {
+            this.xboxGamesSeeking.push(this.resultsSeeking[i]);
+          }
+        }
+      }
+    }.bind(this), function(error){
+      console.error(error);
+    }.bind(this));
+  };
   this.displayPlaystation = function() {
     this.hasXbox = false;
     this.hasPlaystation = true;
@@ -39,6 +58,51 @@ angular
   };
   this.addRecipient = function(recipient) {
     MainService.addRecipient(recipient);
+  };
+  this.getUserInfo = function() {
+    ProfileServices.getProfileData()
+    .then(function(data) {
+      this.userInfo = data;
+    }.bind(this), function(error) {
+      throw error;
+    }.bind(this));
+  };
+  this.compareUserSeekingWithAllOfferings = function(userInfo) {
+    this.psMatches = [];
+    this.xboxMatches = [];
+    for(var i = 0; i < userInfo.seeking.results.length; i++) {
+      this.searchOfferings(userInfo.seeking.results[i].title);
+      if(userInfo.seeking.results[i].platform === 'Playstation 4') {
+        for(var j = 0; j < this.psGames.length; j++) {
+          if(userInfo.seeking.results[i].city === this.psGames[j].city) {
+            this.psMatches.push(this.psGames[j]);
+          }
+        }
+      }
+      if(userInfo.seeking.results[i].platform === 'Xbox One') {
+        for(var k = 0; k < this.xboxGames.length; k++) {
+          if(userInfo.seeking.results[i].city === this.xboxGames[k].city) {
+            this.xboxMatches.push(this.xboxGames[k]);
+          }
+        }
+      }
+    }
+  };
+  this.compareUserOfferingWithEachMatch = function(userInfo) {
+    this.swaps = [];
+    for(var i = 0; i < userInfo.offering.results.length; i++) {
+      this.searchSeeking(userInfo.offering.results[i].title);
+      for(var j = 0; j < this.psGamesSeeking.length; j++) {
+        if(userInfo.offering.results[i].title === this.psGamesSeeking[j].title && userInfo.offering.results[i].platform === 'Playstation 4') {
+          for(var k = 0; k < this.psMatches.length; k++) {
+            if(this.psGamesSeeking[j].username === this.psMatches[k].username) {
+              this.swaps.push(userInfo.offering.results[i]);
+              this.swaps.push(this.psMatches[k]);
+            }
+          }
+        }
+      }
+    }
   };
 
   //TODO: get currentUser's seeking games from profile data and then compare against every game in the offering table
--- client/messages/messagesController.js
+++ client/messages/messagesController.js
@@ -1,15 +1,15 @@
 angular
 .module('messages.controller', [])
 .controller('MessagesController', function(MainService, MessagesService) {
-  this.recipient = MainService.getRecipient();
-  this.author = /*currentUser*/
-  this.sendMessage = function(message) {
-    MessagesService.sendMessage(message)
+  this.to = MainService.getRecipient();
+  this.sendMessage = function(messageText, to) {
+    MessagesService.sendMessage({message: {text: messageText, to: to}})
     .then(function(data) {
+      this.from = data.userfrom;
       MessagesService.getMessages();
-    }, function(error) {
+    }.bind(this), function(error) {
       throw error;
-    });
+    }.bind(this));
   };
   this.getMessages = function() {
     return MessagesService.getMessages();
