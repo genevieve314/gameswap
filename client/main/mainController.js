2fdd427e275add5d0ea0d497b294f4de195706d6
Ryan Reynolds <reynoldsryanscott@gmail.com>
2016-01-15 21:06:13 -0800

    [bugfix] all game swaps showing on screen as expected

--- client/main/mainController.js
+++ client/main/mainController.js
@@ -92,36 +92,38 @@ angular
     this.xboxGames = [];
     this.psMatches = [];
     this.xboxMatches = [];
+    this.ps4Unique = {};
+    this.xboxUnique = {};
     for (var i = 0; i < userInfo.seeking.length; i++) {
       (function (item) {
-      this.searchOfferings(item.title)
-      .then(function() {
-        // setTimeout(function() {
-        // console.log('psGames',this.psGames);
-        // console.log('xboxGames',this.xboxGames);
-        // console.log('i',i);
-        // console.log('item',item);
-        if (item.platform === 'Playstation 4') {
-          for (var j = 0; j < this.psGames.length; j++) {
-            if (userInfo.city === this.psGames[j].city) {
-              this.psMatches.push(this.psGames[j]);
+        this.searchOfferings(item.title)
+        .then(function() {
+          // setTimeout(function() {
+          // console.log('psGames',this.psGames);
+          // console.log('xboxGames',this.xboxGames);
+          // console.log('i',i);
+          // console.log('item',item);
+          if (item.platform === 'Playstation 4') {
+            for (var j = 0; j < this.psGames.length; j++) {
+              if (userInfo.city === this.psGames[j].city) {
+                this.psMatches.push(this.psGames[j]);
+              }
             }
           }
-        }
-        if (item.platform === 'Xbox One') {
-          for (var k = 0; k < this.xboxGames.length; k++) {
-            if (userInfo.city === this.xboxGames[k].city) {
-              this.xboxMatches.push(this.xboxGames[k]);
+          if (item.platform === 'Xbox One') {
+            for (var k = 0; k < this.xboxGames.length; k++) {
+              if (userInfo.city === this.xboxGames[k].city) {
+                this.xboxMatches.push(this.xboxGames[k]);
+              }
             }
           }
-        }
-        // console.log('this.psGamesOffering',this.psGames);
-        // console.log('this.xboxGamesOffering',this.xboxGames);
-        console.log('psMatches',this.psMatches);
-        console.log('xboxMatches',this.xboxMatches);
-        // }.bind(this), 500);
-      }.bind(this));
-    }.bind(this))(userInfo.seeking[i]);
+          console.log('this.psGamesOffering',this.psGames);
+          console.log('this.xboxGamesOffering',this.xboxGames);
+          console.log('psMatches',this.psMatches);
+          console.log('xboxMatches',this.xboxMatches);
+          // }.bind(this), 500);
+        }.bind(this));
+      }.bind(this))(userInfo.seeking[i]);
     }
   };
   // if (this.userInfo) {
@@ -133,51 +135,84 @@ angular
     this.swaps = [];
     for (var i = 0; i < userInfo.offerings.length; i++) {
       (function(item) {
-      this.searchSeeking(userInfo.offerings[i].title)
-      .then(function() {
-        // i = i - 1;
-        console.log('item',item);
+        this.searchSeeking(userInfo.offerings[i].title)
+        .then(function() {
+          // i = i - 1;
+          console.log('item',item);
 
-        for (var j = 0; j < this.psGamesSeeking.length; j++) {
-          console.log('psGamesSeeking', this.psGamesSeeking);
-          if (item.title === this.psGamesSeeking[j].title && item.platform === 'Playstation 4') {
-            for (var k = 0; k < this.psMatches.length; k++) {
-              if (this.psGamesSeeking[j].username === this.psMatches[k].username) {
-                this.swaps.push(this.psMatches[k]);
-                this.swaps.push(item);
+          for (var j = 0; j < this.psGamesSeeking.length; j++) {
+            console.log('psGamesSeeking', this.psGamesSeeking);
+            if (item.title === this.psGamesSeeking[j].title && item.platform === 'Playstation 4') {
+              for (var k = 0; k < this.psMatches.length; k++) {
+                if (this.psGamesSeeking[j].username === this.psMatches[k].username) {
+                  if (this.ps4Unique[this.psMatches[k].createdat] === undefined) {
+                    this.ps4Unique[this.psMatches[k].createdat] = this.psMatches[k].createdat;
+                    console.log('unique ps4 games',this.ps4Unique[this.psMatches[k].createdat]);
+                    this.swaps.push(this.psMatches[k]);
+                    this.swaps.push(item);
+                  }
+                }
               }
-            }
-            for (var o = 0; o < this.xboxMatches.length; o++) {
-              if (this.psGamesSeeking[j].username === this.xboxMatches[o].username) {
-                this.swaps.push(this.xboxMatches[o]);
-                this.swaps.push(item);
-              }
-            }
-          }
-        }
-        for (var l = 0; l < this.xboxGamesSeeking.length; l++) {
-          console.log('xboxGamesSeeking', this.xboxGamesSeeking);
-          // console.log('i',i);
-          // console.log('i',i);
-          if (item.title === this.xboxGamesSeeking[l].title && item.platform === 'Xbox One') {
-            for (var m = 0; m < this.psMatches.length; m++) {
-              if (this.xboxGamesSeeking[l].username === this.psMatches[m].username) {
-                this.swaps.push(this.psMatches[m]);
-                this.swaps.push(item);
-              }
-            }
-            for (var n = 0; n < this.xboxMatches.length; n++) {
-              if (this.xboxGamesSeeking[l].username === this.xboxMatches[n].username) {
-                this.swaps.push(this.xboxMatches[n]);
-                this.swaps.push(item);
+              for (var o = 0; o < this.xboxMatches.length; o++) {
+                if (this.psGamesSeeking[j].username === this.xboxMatches[o].username) {
+                  if (this.ps4Unique[this.xboxMatches[o].createdat] === undefined) {
+                    this.ps4Unique[this.xboxMatches[o].createdat] = this.xboxMatches[o].createdat;
+                    this.swaps.push(this.xboxMatches[o]);
+                    this.swaps.push(item);
+                  }
+                }
               }
             }
           }
-        }
-        console.log('swaps',this.swaps);
-      }.bind(this));
-    }.bind(this))(userInfo.offerings[i]);
+          for (var l = 0; l < this.xboxGamesSeeking.length; l++) {
+            console.log('xboxGamesSeeking', this.xboxGamesSeeking);
+            // console.log('i',i);
+            // console.log('i',i);
+            if (item.title === this.xboxGamesSeeking[l].title && item.platform === 'Xbox One') {
+              for (var m = 0; m < this.psMatches.length; m++) {
+                if (this.xboxGamesSeeking[l].username === this.psMatches[m].username) {
+                  if (this.xboxUnique[this.psMatches[m].createdat] === undefined) {
+                    this.xboxUnique[this.psMatches[m].createdat] = this.psMatches[m].createdat;
+                    this.swaps.push(this.psMatches[m]);
+                    this.swaps.push(item);
+                  }
+                }
+              }
+              for (var n = 0; n < this.xboxMatches.length; n++) {
+                if (this.xboxGamesSeeking[l].username === this.xboxMatches[n].username) {
+                  if (this.xboxUnique[this.xboxMatches[n].createdat] === undefined) {
+                    this.xboxUnique[this.xboxMatches[n].createdat] = this.xboxMatches[n].createdat;
+                    this.swaps.push(this.xboxMatches[n]);
+                    this.swaps.push(item);
+                  }
+                }
+              }
+            }
+          }
+          console.log('swaps',this.swaps);
+          this.first = 0;
+          this.second = 1;
+        }.bind(this));
+      }.bind(this))(userInfo.offerings[i]);
     }
+  };
+
+  this.previous = function() {
+    if(this.first > 0) {
+      this.first -= 2;
+      this.second -= 2;
+    }
+    console.log(this.first);
+    console.log(this.second);
+  };
+
+  this.next = function() {
+    if(this.second < this.swaps.length - 1) {
+      this.first += 2;
+      this.second += 2;
+    }
+    console.log(this.first);
+    console.log(this.second);
   };
 
   if (this.isAuth) {
@@ -192,7 +227,7 @@ angular
       this.compareUserOfferingWithEachMatch(data);
     }.bind(this));
   }
-//   if (this.userInfo) {
-//     this.compareUserOfferingWithEachMatch(this.userInfo);
-//   }
+  //   if (this.userInfo) {
+  //     this.compareUserOfferingWithEachMatch(this.userInfo);
+  //   }
 });
