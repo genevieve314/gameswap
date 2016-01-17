3fbd013e98129a470a28f9cd4b04bcb6e8739f0c
Thomas Leupp <tomleupp@gmail.com>
2016-01-16 17:52:29 -0800

    fixing header that isnt updating on state change

--- client/indexController.js
+++ client/indexController.js
@@ -1,11 +1,30 @@
 angular
 .module('index.controller', [])
-.controller('IndexController', function(AuthServices) {
-  if (AuthServices.isAuth()) {
-    this.isAuth = true;
-  } else {
-    this.isAuth = false;
-  }
+.controller('IndexController', function(AuthServices, IndexServices) {
+
+	console.log('this.isAuth in IndexController: ', this.isAuth);
+    if (AuthServices.isAuth()) {
+      this.isAuth = true;
+      console.log('this.isAuth in IndexController: ', this.isAuth);
+    } else {
+      this.isAuth = false;
+      console.log('this.isAuth in IndexController: ', this.isAuth);
+    }  
+ 
+
+  // var toggleState = function(){
+  //   if(AuthServices.isAuth()){
+  //       this.isAuth = false;
+  //       AuthServices.signOut();
+  //   } else {
+  //       this.isAuth = true;
+  // }
+  //   }
+
+  // checkState();
+  IndexServices.checkState();
+
+  
   this.signOut = function(){
     AuthServices.signOut();
   };
